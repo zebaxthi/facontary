14296c055a33fba8670c2a165ac8661c6c72e85e
Sebastian Aguirre <sebastian.aguirre@wiga.io>
2022-06-01 16:05:18 -0500

    feat: diseño de tabla inventario

--- src/app/modules/inventario/inventario.module.ts
+++ src/app/modules/inventario/inventario.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {TableModule} from 'primeng/table';
+import {ToastModule} from 'primeng/toast';
+import {CalendarModule} from 'primeng/calendar';
+import {SliderModule} from 'primeng/slider';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {ContextMenuModule} from 'primeng/contextmenu';
+import {DialogModule} from 'primeng/dialog';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {InputTextModule} from 'primeng/inputtext';
+import {FileUploadModule} from 'primeng/fileupload';
+import {ToolbarModule} from 'primeng/toolbar';
+import {RatingModule} from 'primeng/rating';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {InputNumberModule} from 'primeng/inputnumber';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarTopModule } from 'src/app/shared/navbar-top/navbar-top.module';
+import { NavbarLeftModule } from 'src/app/shared/navbar-left/navbar-left.module';
 import { InventarioRoutingModule } from './inventario-routing.module';
-import { RouterModule } from '@angular/router';
 import { InventarioComponent } from './inventario.component';
-import { NavbarLeftModule } from '../../shared/navbar-left/navbar-left.module';
-import { NavbarTopModule } from '../../shared/navbar-top/navbar-top.module';
+import { InventarioService } from './services/inventario.service';
 
 
 @NgModule({
@@ -14,7 +36,28 @@ import { NavbarTopModule } from '../../shared/navbar-top/navbar-top.module';
     CommonModule,
     InventarioRoutingModule,
     NavbarLeftModule,
-    NavbarTopModule
+    NavbarTopModule,
+    TableModule,
+    CalendarModule,
+		SliderModule,
+		DialogModule,
+		MultiSelectModule,
+		ContextMenuModule,
+		DropdownModule,
+		ButtonModule,
+		ToastModule,
+    InputTextModule,
+    ProgressBarModule,
+    HttpClientModule,
+    FileUploadModule,
+    ToolbarModule,
+    RatingModule,
+    FormsModule,
+    RadioButtonModule,
+    InputNumberModule,
+    ConfirmDialogModule,
+    InputTextareaModule,
   ],
+  providers: [InventarioService, MessageService, ConfirmationService]
 })
 export class InventarioModule { }
